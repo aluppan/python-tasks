8a19ac63057be5534fb9bcae5ecd57a537435747
aluppan <38321518+aluppan@users.noreply.github.com>
2023-04-18 00:18:41 +0400

    Add files via upload

---
+++ tasks/task-02.py
@@ -0,0 +1,23 @@
+#Задание 1
+# num1 = int(input("number - "))
+# num2 = int(input("percent - "))
+# print("result = ", num1 * num2 / 100)
+
+#Задание 2
+# salary = int(input("salary - "))
+# debt_bank = int(input("debt to the bank - "))
+# arrears = int(input("arrears - "))
+# print("balance = ", salary - debt_bank - arrears)
+
+#Задание 3
+# a = int(input("height - "))
+# b = int(input("width - "))
+# s = a * b
+# print("area - ", s)
+
+#Задание 4
+# a1 = int(input("diagonal 1 - "))
+# b1 = int(input("diagonal 2 - "))
+# s1 = a1 * b1 / 2
+# print("area - ", s1)
+
