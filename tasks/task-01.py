d07eaa3499ed3d91cdc735d281963b44838c11b2
aluppan <38321518+aluppan@users.noreply.github.com>
2023-04-18 00:18:25 +0400

    Add files via upload

---
+++ tasks/task-01.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter number 1 "))
+num2 = int(input("enter number 2 "))
+
+print("addition -", num1 + num2)
+print("subtraction -", num1 - num2)
+print("multiplication -", num1 * num2)
+print("division -", num1 / num2)
