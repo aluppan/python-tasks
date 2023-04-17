0a2006bd581214c74553fb0e5d43a07ff2b16e58
aluppan <38321518+aluppan@users.noreply.github.com>
2023-04-18 00:21:12 +0400

    Add files via upload

---
+++ tasks/task-08.py
@@ -0,0 +1,41 @@
+# #Задание 1
+# print("\\\\", '\t', "//")
+# print(" \\\\", "//")
+# print(" //", "\\\\")
+# print("//", '\t', "\\\\")
+#
+# print("\t", "   __")
+# print("\t" "  //", " \\\\")
+# print("\t", "//", "  ", "\\\\")
+# print("   ", "//", "____", "\\\\")
+# print("  ", "//", "\t\t", "\\\\", "\n")
+
+# #Задание 2
+# print("This text")
+# print("\t\t\t", "contains many tabs")
+# print("\t\t\t\t\t\t\t", "new lines")
+# print("\t\t\t\t\t\t\t", "and spaces!", "\n")
+# print("Have fun!")
+
+# # Задание 3
+# text1 = input()
+# b = input()
+# for b in text1:
+#     print(b)
+#     break
+
+# #Задание 4
+# text2 = input()
+# begin = int(input())
+# end = int(input())
+# print(text2[begin:end])
+
+# #Задание 5
+# text2 = input("text - ")
+# text3 = ""
+# for char in text2:
+#     if char == " " or char == "\t":
+#         char == ""
+#         continue
+#     text3 += char
+# print(text3)
